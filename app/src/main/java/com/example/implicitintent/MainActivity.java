6213631ef389239cad62b2e14bae02ecf331cd3f
Chan Nghiep <ncnghiep1705@gmail.com>
2019-10-30 17:34:24 -0700

    edit

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,25 +17,27 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<listItem> urlList;
-
+    static ArrayList<listItem> urlList = new ArrayList<listItem>();
+    String data;
+    listItem tempItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("state",urlList.toString());
 
-        urlList = new ArrayList<listItem>();
-        listItem tempItem = new listItem();
+//        urlList
+        tempItem = new listItem();
 
         Intent i = getIntent();
 
-        String data = i.getStringExtra(Intent.EXTRA_TEXT);
+        data = i.getStringExtra(Intent.EXTRA_TEXT);
 
         if(data != null) {
-            Log.e("test", data);
+//            Log.e("test", data);
             tempItem.url = data;
-            Log.e("test2", tempItem.url.toString());
+//            Log.e("test2", tempItem.url.toString());
             urlList.add(tempItem);
 
         }
@@ -42,8 +46,54 @@ public class MainActivity extends AppCompatActivity {
 //        ArrayAdapter<listItem> adapter = new ArrayAdapter<listItem>(
 //                this, android.R.layout.simple_list_item_1, urlList);
 //        listView.setAdapter(adapter);
-
+        Log.d("state","create");
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        Log.e("test", "paused");
+        Log.e("test array pause", urlList.toString());
+
+
+
+//        urlList.add(tempItem);
+
+        Log.d("state","paused");
+        Log.d("state",urlList.toString());
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+//        setupAdapter();
+//
+//        Intent i = getIntent();
+//
+//        data = i.getStringExtra(Intent.EXTRA_TEXT);
+//
+//
+////        urlList.add(tempItem);
+//        if(data != null) {
+////            Log.e("test", data);
+//            tempItem.url = data;
+////            Log.e("test2", tempItem.url.toString());
+//            urlList.add(tempItem);
+//
+//        }
+
+//        setupAdapter();
+
+        Log.e("test", "resume");
+        Log.e ("test array resume", urlList.toString());
+
+
+
+        Log.d("state","resumed");
+        Log.d("state",urlList.toString());
+    }
+
+
 
     private void setupAdapter(){
 
@@ -57,9 +107,22 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 listItem item = (listItem) adapter.getItem(position);
 
-                Intent intent = new Intent(MainActivity.this, webview.class);
-                intent.putExtra("url", item.getUrl());
-                startActivity(intent);
+                WebView webView = findViewById(R.id.webView);
+
+
+                webView.getSettings().setJavaScriptEnabled(true);
+
+                webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        super.onPageFinished(view, url);
+                        Log.d("WEBVIEW", "Finished loading " + url);
+                    }
+                });
+
+                Log.d("url", urlList.get(0).getUrl());
+                webView.loadUrl(urlList.get(position).getUrl());
+
             }
         });
     }
